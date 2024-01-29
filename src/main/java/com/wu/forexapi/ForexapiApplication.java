b0ade87bd4ee586b90a45966967cbd2d059bbbe7
pkomikomet <pekomikomet@gmail.com>
2024-01-29 17:42:44 +0800

    git init

---
+++ src/main/java/com/wu/forexapi/ForexapiApplication.java
@@ -0,0 +1,13 @@
+package com.wu.forexapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForexapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForexapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wu/forexapi/ForexapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wu.forexapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForexapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
